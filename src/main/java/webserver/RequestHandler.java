8ea2c45d474cecfbb32430e45054df8a9d3b5b02
LEEHYUNDONG <l.steve10@yahoo.com>
2023-06-16 13:19:58 +0900

    feat: Webserver request handler v1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,11 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,29 +28,46 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
             // InputStream을 한 줄 단위로 읽기 위해 BufferedReader 생성
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String line = br.readLine();
-            while (!"".equals(line)){
-                if(isNull(line)){
-                    return;
-                }
-                System.out.println(line);
-                line = br.readLine();
+            DataOutputStream dos = new DataOutputStream(out);
+            String url = readBuffer(in);
+            byte[] body = fileToByte(url);
+
+            if(url.contains("css") && !url.contains("map")){
+                setCssAndResponse(dos, body.length);
+                setResponseBody(dos, body);
+            }
+            if(url.contains("html")){
+                response200Header(dos, body.length);
+                setResponseBody(dos, body);
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            String url = "/index.html";
 
-            byte[] body = fileToByte(url);
-            response200Header(dos, body.length);
-            setResponseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private boolean isNull(String line){
+    public String readBuffer(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+        String url = "";
+        while (!"".equals(line)) {
+            if (isNull(line)) {
+                return "";
+            }
+            String[] token = line.split(" ");
+            if(token[0].equals("GET")){
+                url = token[1];
+            }
+            System.out.println(line);
+            line = br.readLine();
+        }
+        return url;
+    }
+
+    private boolean isNull(String line) {
         return line == null;
     }
 
@@ -69,10 +91,18 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private byte[] fileToByte(String url) throws IOException{
+    private byte[] fileToByte(String url) throws IOException {
         return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
-    private void setResponseBody(DataOutputStream dos, byte[] body) throws IOException{
+
+    private void setCssAndResponse(DataOutputStream dos, int lengthOfBodyContent) throws IOException{
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private void setResponseBody(DataOutputStream dos, byte[] body) throws IOException {
         dos.write(body, 0, body.length);
         dos.flush();
     }

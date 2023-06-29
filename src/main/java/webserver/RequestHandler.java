63c42b6dd19ca0a12ea9a81d1d11c032dc0ea4c7
LEEHYUNDONG <l.steve10@yahoo.com>
2023-06-29 21:49:56 +0900

    Login 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,8 +9,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -28,19 +32,51 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
 
-            // InputStream을 한 줄 단위로 읽기 위해 BufferedReader 생성
-            DataOutputStream dos = new DataOutputStream(out);
-            String url = readBuffer(in);
-            byte[] body = fileToByte(url);
-
-            if(url.contains("css") && !url.contains("map")){
-                setCssAndResponseHeader(dos, body.length);
-                setResponseBody(dos, body);
+            if (line == null) {
+                return;
             }
-            if(url.contains("html")){
-                response200Header(dos, body.length);
-                setResponseBody(dos, body);
+
+            String[] tokens = line.split(" ");
+            int contentLength = 0;
+
+            while (!line.equals("")) {
+                log.debug("header : {}", line);
+                line = br.readLine();
+                if (line.contains("Content-Length")) {
+                    contentLength = getContentLength(line);
+                }
+            }
+
+            String url = tokens[1];
+
+            if (url.startsWith("/user/create")) {
+                String body = IOUtils.readData(br, contentLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user.toString());
+                url = "/index.html";
+                DataOutputStream dos = new DataOutputStream(out);
+                response300Header(dos, "/index.html");
+                DataBase.addUser(user);
+
+            } else if ("/user/login".equals(url)) {
+                String body = IOUtils.readData(br, contentLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+                User user = DataBase.findUserById(params.get("userId"));
+                if (user == null) {
+                    responseResource(out, "/user/login_failed.html");
+                    return;
+                }
+                if (user.getPassword().equals(params.get("password"))) {
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302LoginSuccessHeader(dos);
+                }
+            } else {
+                responseResource(out, url);
             }
 
 
@@ -49,28 +85,31 @@ public class RequestHandler extends Thread {
         }
     }
 
-    public String readBuffer(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String line = br.readLine();
-        String url = "";
-        if (isNull(line)) {
-            return "";
-        }
-        String[] token = line.split(" ");
-        if(token[0].equals("GET")){
-            url = token[1];
-        }
+    private void responseResource(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
 
-        while (!"".equals(line)) {
-            log.debug("header: {}",line);
-            line = br.readLine();
-        }
-        return url;
     }
 
-    private boolean isNull(String line) {
-        return line == null;
+    private void response302LoginSuccessHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Set-Cookie: logined=true \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
+
+
+    private int getContentLength(String line) {
+        String[] headerTokens = line.split(":");
+        return Integer.parseInt(headerTokens[1].trim());
+    }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
@@ -83,7 +122,17 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void responseHeader(DataOutputStream dos, int lengthOfBodyContent) throws IOException{
+    private void response300Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 REDIRECT \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseHeader(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
 
 
     }
@@ -101,7 +150,7 @@ public class RequestHandler extends Thread {
         return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
 
-    private void setCssAndResponseHeader(DataOutputStream dos, int lengthOfBodyContent) throws IOException{
+    private void setCssAndResponseHeader(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
         dos.writeBytes("HTTP/1.1 200 OK \r\n");
         dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
         dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");

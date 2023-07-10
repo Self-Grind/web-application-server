5c790635a9512281ea760366fcfacd7f56e1a493
LEEHYUNDONG <l.steve10@yahoo.com>
2023-07-10 22:37:43 +0900

    POST 요청 처리 및 테스트 작성

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -23,4 +23,15 @@ public class HttpRequestTest {
         assertEquals("javajigi", request.getParameter("userId"));
         
     }
+
+    @Test
+    public void request_POST() throws Exception{
+        InputStream in = Files.newInputStream(new File(testDirectory + "Http_POST.txt").toPath());
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals("POST", request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParameter("userId"));
+    }
 }
